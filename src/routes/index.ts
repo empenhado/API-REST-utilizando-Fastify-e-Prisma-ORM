02daf1cd24a1d4ccfd3f7aa2744be07c9d8ac2c0
Ester Martins <sterre.msb@gmail.com>
2026-04-08 23:25:38 -0300

    Add comentário em index.ts
    Added a comment explaining the registration of application routes.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,8 @@ import { clienteRoutes } from "./cliente.routes";
 import { produtoRoutes } from "./produto.routes";
 import { pedidoRoutes } from "./pedido.routes";
 
+  ///Registrando todas as rotas da aplicação no servidor fastify, sendo /clientes, /produtos e /pedidos
+
 export async function appRoutes(app: FastifyInstance) {
   app.register(clienteRoutes, { prefix: "/clientes" });
   app.register(produtoRoutes, { prefix: "/produtos" });
